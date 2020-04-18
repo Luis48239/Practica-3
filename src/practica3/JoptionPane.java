1b687c6d45f513f389394f685f2a1e5d62afcd76
luis3 <luis3@LAPTOP-I5BIBL0E>
2020-04-17 22:32:52 -0500

    practica 3

---
+++ src/practica3/JoptionPane.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica3;
+
+/**
+ */
+class JoptionPane {
+
+    static void showMessageDialog(Object object, String selecciona_una) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
